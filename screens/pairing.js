ddd085793fd53e77f748b69220b33ac1549aee7f
tperale <thomas.perale@openmailbox.org>
2019-02-01 00:34:34 +0100

    [add] refresh for the bluetooth device

--- screens/pairing.js
+++ screens/pairing.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Container, Title, Body, Header, Content, Spinner, Button, Text, List, ListItem, } from 'native-base';
 import BluetoothSerial from 'react-native-bluetooth-serial-next';
+import { AsyncStorage, ScrollView, RefreshControl } from 'react-native';
 import { Subscribe } from 'unstated';
 
 import { ScreensContainer, DeviceContainer } from '../containers';
@@ -14,14 +15,15 @@ export default class Pairing extends Component {
   constructor(props) {
     super(props);
 
-
     this.state = {
       status: PENDING,
       devices: [],
+      refreshing: true,
     };
 
     this.discover = this.discover.bind(this);
     this.pair = this.pair.bind(this);
+    this._onRefresh = this._onRefresh.bind(this);
   }
 
   componentDidMount() {
@@ -32,6 +34,7 @@ export default class Pairing extends Component {
     if (this.state.status !== DISCOVERING) {
       this.setState({
         status: DISCOVERING,
+        refreshing: false,
       });
 
       try {
@@ -39,11 +42,13 @@ export default class Pairing extends Component {
         this.setState({
           devices,
           status: SUCCESS,
+          refreshing: false,
         });
       } catch (err) {
         console.warn(err);
         this.setState({
           status: FAILED,
+          refreshing: false,
         });
       }
     }
@@ -53,49 +58,55 @@ export default class Pairing extends Component {
     try {
       const paired = await BluetoothSerial.device(device.id).connect();
       container.set(paired);
+      await AsyncStorage.setItem("pairedDevice", device.id);
       screens.receipt();
     } catch (err) {
-      return console.warn(container.state.current);
+      return console.warn(err);
     }
+  }
+
+  _onRefresh() {
+    this.setState({ refreshing: true });
+    this.discover();
   }
 
   render() {
     return (
       <Subscribe to={[ScreensContainer, DeviceContainer]}>
         {(screens, device) => (
-          <Container>
-            <Header>
-              <Body>
-                <Title>Connect to printer</Title>
-              </Body>
-            </Header>
-            <Content>
-              {
-                (this.state.status === DISCOVERING) || (this.state.status === PENDING) ?
-                  <Spinner />
-                : this.state.status === FAILED ?
-                  <Content>
-                    <Text>The bluetooth discover failed.</Text>
-                    <Button primary onPress={() => this.discover()}>
-                      <Text>
-                        Retry
-                      </Text>
-                    </Button>
-                  </Content>
-                :
-                  <List>
-                    {this.state.devices.map(x => (
-                      <ListItem thumbnail key={x.id} onPress={() => this.pair(x, device, screens)}>
-                        <Body>
-                          <Text>{x.name}</Text>
-                          <Text note numberOfLines={1}>{x.id}</Text>
-                        </Body>
-                      </ListItem>
-                    ))}
-                  </List>
-              }
-            </Content>
-          </Container>
+          <ScrollView
+            refreshControl={
+              <RefreshControl
+                refreshing={this.state.refreshing}
+                onRefresh={this._onRefresh}
+              />
+            }
+          >
+            {
+              (this.state.status === DISCOVERING) || (this.state.status === PENDING) ?
+                <Spinner />
+              : this.state.status === FAILED ?
+                <Content>
+                  <Text>The bluetooth discover failed.</Text>
+                  <Button primary onPress={() => this.discover()}>
+                    <Text>
+                      Retry
+                    </Text>
+                  </Button>
+                </Content>
+              :
+                <List>
+                  {this.state.devices.map(x => (
+                    <ListItem thumbnail key={x.id} onPress={() => this.pair(x, device, screens)}>
+                      <Body>
+                        <Text>{x.name}</Text>
+                        <Text note numberOfLines={1}>{x.id}</Text>
+                      </Body>
+                    </ListItem>
+                  ))}
+                </List>
+            }
+          </ScrollView>
         )}
       </Subscribe>
     );
